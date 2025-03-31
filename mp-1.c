48ee27c0b3797a2f0fb99555a2779f871953af5a
Leonard Ang <117495538+drew-747@users.noreply.github.com>
2025-03-31 20:09:05 +0800

    Update mp-1.c

--- mp-1.c
+++ mp-1.c
@@ -3,8 +3,6 @@
 #include <string.h>
 #include <math.h>
 
-#define EPSILON 1e-9
-
 typedef struct Term {
     int exp_x;
     int exp_y;
@@ -24,10 +22,6 @@ void insertTerm(Polynomial *p, int ex, int ey, int ez, float c);
 Polynomial readPolynomial();
 void printPolynomial(Polynomial p);
 Polynomial addPolynomial(Polynomial p1, Polynomial p2);
-Polynomial subtractPolynomial(Polynomial p1, Polynomial p2);
-// Polynomial multiplyPolynomial(Polynomial p1, Polynomial p2);
-// Polynomial dividePolynomial(Polynomial A, Polynomial B);
-// Polynomial moduloPolynomial(Polynomial A, Polynomial B);
 
 
 Polynomial createPolynomial() {
@@ -74,6 +68,7 @@ void insertTerm(Polynomial *p, int ex, int ey, int ez, float c) {
 
     while (current != NULL) {
         int cmp = compareExponents(ex, ey, ez, current->exp_x, current->exp_y, current->exp_z);
+
         if (cmp > 0) {
             break;
         } else if (cmp == 0) {
@@ -109,6 +104,7 @@ Polynomial readPolynomial() {
 
     if (scanf("%d", &n) != 1) {
          fprintf(stderr, "Error: Failed to read number of terms.\n");
+         destroyPolynomial(&p);
          exit(EXIT_FAILURE);
     }
 
@@ -117,6 +113,7 @@ Polynomial readPolynomial() {
         float c;
         if (scanf("%d %d %d %f", &ex, &ey, &ez, &c) != 4) {
              fprintf(stderr, "Error: Failed to read term %d.\n", i + 1);
+             destroyPolynomial(&p);
              exit(EXIT_FAILURE);
         }
         insertTerm(&p, ex, ey, ez, c);
@@ -179,7 +176,11 @@ Polynomial addPolynomial(Polynomial p1, Polynomial p2) {
 
         if (fabs(newCoeff) >= EPSILON) {
              Term *newNode = (Term *)malloc(sizeof(Term));
-             if (!newNode) { fprintf(stderr, "Oops, malloc failed in addPolynomial.\n"); exit(EXIT_FAILURE); }
+             if (!newNode) {
+                 fprintf(stderr, "Oops, malloc failed in addPolynomial.\n");
+                 destroyPolynomial(&result);
+                 exit(EXIT_FAILURE);
+             }
              newNode->exp_x = ex; newNode->exp_y = ey; newNode->exp_z = ez;
              newNode->coeff = newCoeff;
              newNode->next = NULL;
@@ -196,59 +197,6 @@ Polynomial addPolynomial(Polynomial p1, Polynomial p2) {
     return result;
 }
 
-Polynomial subtractPolynomial(Polynomial p1, Polynomial p2) {
-    Polynomial result = createPolynomial();
-    Term *tail = NULL;
-    Term *ptr1 = p1.head;
-    Term *ptr2 = p2.head;
-
-    while (ptr1 != NULL || ptr2 != NULL) {
-        int ex = 0, ey = 0, ez = 0;
-        float newCoeff = 0.0;
-        int cmp = 0;
-
-        if (ptr1 != NULL && ptr2 != NULL) {
-            cmp = compareExponents(ptr1->exp_x, ptr1->exp_y, ptr1->exp_z,
-                                   ptr2->exp_x, ptr2->exp_y, ptr2->exp_z);
-        } else if (ptr1 != NULL) {
-            cmp = 1;
-        } else {
-            cmp = -1;
-        }
-
-         if (cmp > 0) {
-            ex = ptr1->exp_x; ey = ptr1->exp_y; ez = ptr1->exp_z;
-            newCoeff = ptr1->coeff;
-            ptr1 = ptr1->next;
-        } else if (cmp < 0) {
-            ex = ptr2->exp_x; ey = ptr2->exp_y; ez = ptr2->exp_z;
-            newCoeff = -ptr2->coeff;
-            ptr2 = ptr2->next;
-        } else {
-            ex = ptr1->exp_x; ey = ptr1->exp_y; ez = ptr1->exp_z;
-            newCoeff = ptr1->coeff - ptr2->coeff;
-            ptr1 = ptr1->next;
-            ptr2 = ptr2->next;
-        }
-
-        if (fabs(newCoeff) >= EPSILON) {
-             Term *newNode = (Term *)malloc(sizeof(Term));
-             if (!newNode) { fprintf(stderr, "Oops, malloc failed in subtractPolynomial.\n"); exit(EXIT_FAILURE); }
-             newNode->exp_x = ex; newNode->exp_y = ey; newNode->exp_z = ez;
-             newNode->coeff = newCoeff;
-             newNode->next = NULL;
-
-             if (tail == NULL) {
-                 result.head = newNode;
-                 tail = newNode;
-             } else {
-                 tail->next = newNode;
-                 tail = newNode;
-             }
-        }
-    }
-    return result;
-}
 
 int main() {
     char op;
@@ -266,28 +214,8 @@ int main() {
                 processed = 1;
                 break;
 
-            case '-':
-                result = subtractPolynomial(p1, p2);
-                processed = 1;
-                break;
-
-            case '*':
-                printf("---\n");
-                printf("Multiplication not implemented yet.\n");
-                break;
-
-            case '/':
-                 printf("---\n");
-                 printf("Division not implemented yet.\n");
-                break;
-
-            case '%':
-                 printf("---\n");
-                 printf("Modulo not implemented yet.\n");
-                 break;
-
             default:
-                fprintf(stderr, "Error: Invalid operation symbol '%c'.\n", op);
+                fprintf(stderr, "Error: Invalid operation symbol '%c' encountered (only '+' expected).\n", op);
                 break;
         }
 
